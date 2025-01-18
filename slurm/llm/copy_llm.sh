a4cb5dc741e11ffee7fa973e7ec3a980ff10c318
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2025-01-18 14:26:08 +0100

    script to copy outputs to GPT-normalized corpora directories

---
+++ slurm/llm/copy_llm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source $HOME/.bashrc 
+source $HOME/.bash_profile
+
+set -e
+
+EXPERIMENT_DIR=$EXPERIMENTS/evaluation-challenges/llm/experiment_049
+
+# Models
+
+MODEL_NAME[0]=meta-llama/Llama-3.1-8B-Instruct
+MODEL_NAME[1]=google/gemma-2-9b-it
+MODEL_NAME[2]=facebook/nllb-200-3.3B
+
+# Corpora
+
+CORPUS[0]=rocsmt
+CORPUS[1]=footweets
+CORPUS[2]=mmtc
+CORPUS[3]=pfsmb
+
+for i in {0..2}
+do
+    echo "Model: ${MODEL_NAME[$i]}"
+
+    for j in {0..3}
+    do
+        echo " - Corpus: ${CORPUS[$j]}"
+        
+        OUTPUT_DIR=$EXPERIMENT_DIR/outputs/${MODEL_NAME[$i]}/${CORPUS[$j]}
+        
+        mkdir -p ${OUTPUT_DIR}-gpt
+        cp -v $OUTPUT_DIR/*.out ${OUTPUT_DIR}-gpt
+    done
+done
+
+echo "Done copying LLM and NLLB outputs to GPT-normalized corpora directories..."
