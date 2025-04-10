c4763c9a49453bcc4681484a17d35b3cfeddade9
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2025-04-11 00:25:07 +0200

    remove xcomet

--- src/llm/comet-robustness.py
+++ src/llm/comet-robustness.py
@@ -25,20 +25,20 @@ for src_lang, tgt_lang in itertools.permutations(["en", "fr"], 2):
 
 comet_model_path = download_model("Unbabel/wmt22-comet-da")
 comet_model = load_from_checkpoint(comet_model_path)
-xcomet_model_path = download_model("Unbabel/XCOMET-XL")
-xcomet_model = load_from_checkpoint(xcomet_model_path)
+# xcomet_model_path = download_model("Unbabel/XCOMET-XL")
+# xcomet_model = load_from_checkpoint(xcomet_model_path)
 
 data = [{"src": src, "mt": mt, "ref": ref} for src, mt, ref in zip(sources, outputs, references)]
 
 scores = {
     "comet": comet_model.predict(data, batch_size=32, gpus=1),
-    "xcomet": xcomet_model.predict(data, batch_size=32, gpus=1)
+    # "xcomet": xcomet_model.predict(data, batch_size=32, gpus=1)
 }
 
 output_dir = f"{os.environ['DATASETS']}/evaluation-challenges/experiment_049/analysis"
 os.makedirs(output_dir, exist_ok=True)
 
-write_json(f"{output_dir}/comet_scores.json", scores["comet"])
+write_json(f"{output_dir}/comet_scores.json", scores)
 
 
 
